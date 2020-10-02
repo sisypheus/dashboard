5271f94a34cf2459e58503d1166c5e7532aa88a5
tpoette <theo.poette@epitech.eu>
2020-10-02 17:02:06 +0200

    [+] repo setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <p>{{url}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+    data() {
+      return {
+        url: 'test'
+      }
+    }
+}
+</script>
+
