db9ca8ca172c6634626e70bef4e4ab2c550ddb96
tpoette <theo.poette@epitech.eu>
2020-10-27 23:37:21 +0100

    [+] messing around with promises

--- scraper/config.js
+++ scraper/config.js
@@ -12,4 +12,46 @@ const config = {
     agent: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1'
 }
 
-module.exports = config;
+class token {
+    constructor(config) {
+        this.config = config;
+    }
+
+    get_token() {
+        rp({
+            uri: `https://sync.bankin.com/v2/authenticate`,
+            method: 'POST',
+            headers: {
+                'User-Agent': this.config.agent,
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'Bankin-Version': this.config.version,
+                'Bankin-Device': this.config.device,
+                'Client-id': this.config.user.id,
+                'Client-secret': this.config.user.secret
+            },
+            body: {
+                'client-secret': this.config.user.secret,
+                'client-id': this.config.user.id,
+                'email': this.config.user.email,
+                'password': this.config.user.password
+            },
+            json: true
+        })
+        .then((response) => {
+            console.log((response.access_token));
+            return response.access_token;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+}
+
+token = new token(config);
+
+module.exports = Object.freeze({
+    config: config,
+    auth_token: token
+})
+//module.exports = config, token;
--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,13 +1,14 @@
 const rp = require('request-promise');
-const config = require('./config');
+const config = require('./config').config;
+const token = require('./config').auth_token;
 
-let auth_token = (() => {
+/*let auth_token = (() => {
     rp({
         uri: `https://sync.bankin.com/v2/authenticate`,
         method: 'POST',
         headers: {
             'User-Agent': config.agent,
-            'Accept': '*/*',
+            'Accept': '*//*',
             'Content-Type': 'application/json',
             'Bankin-Version': config.version,
             'Bankin-Device': config.device,
@@ -29,10 +30,40 @@ let auth_token = (() => {
     .catch((err) => {
         console.log(err);
     });
-});
+});*/
 
-let html = (() => {
-    console.log('zebi');
-});
+function get_token(config) {
+    return new Promise((resolve, reject) => {
+        rp({
+            uri: `https://sync.bankin.com/v2/authenticate`,
+            method: 'POST',
+            headers: {
+                'User-Agent': config.agent,
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'Bankin-Version': config.version,
+                'Bankin-Device': config.device,
+                'Client-id': config.user.id,
+                'Client-secret': config.user.secret
+            },
+            body: {
+                'client-secret': config.user.secret,
+                'client-id': config.user.id,
+                'email': config.user.email,
+                'password': config.user.password
+            },
+            json: true
+        })
+        .then((response) => {
+            console.log((response.access_token));
+            return response.access_token;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
+}
 
-auth_token();
+get_token(config).then((token) => {
+    console.log(token);
+});
