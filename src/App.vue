0c3aa450384a74081f6e5b7b71ad6a1b28a37f38
tpoette <theo.poette@epitech.eu>
2020-11-06 23:28:44 +0100

    [+] design and sweat icon

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,49 @@
   <div id="app">
     <div id="welcome">
       <p ref="greeting" id="greeting"></p>
+      <img id="text_styling" src="@/assets/img/style_el.svg" />
     </div>
 
     <div id="weather">
-      <img id="weather_image" v-if="weather_image != null" v-bind:src="require(`@/assets/img/${weather_image}`)"/>
+      <img
+        id="weather_image"
+        v-if="weather_image != null"
+        v-bind:src="require(`@/assets/img/${weather_image}`)"
+      />
       <div id="weather_container">
-        <p v-if="weather.name && weather.sys && weather.sys.country">{{weather.name}}, {{weather.sys.country}}</p>
-        <p v-if="weather.main && weather.main.temp">{{Math.round(weather.main.temp)}} °C</p>
-        <p v-if="weather.weather && weather.weather[0].main">{{weather.weather[0].main}}</p>
+        <p v-if="weather.name && weather.sys && weather.sys.country">
+          {{ weather.name }}, {{ weather.sys.country }}
+        </p>
+        <p v-if="weather.main && weather.main.temp">
+          {{ Math.round(weather.main.temp) }} °C
+        </p>
+        <p v-if="weather.weather && weather.weather[0].main">
+          {{ weather.weather[0].main }}
+        </p>
       </div>
     </div>
 
     <div id="bank">
       <h2>Bank status</h2>
-      <p v-if="balance != null">{{balance}}€, careful with that budget!</p>
+      <p v-if="balance != null">{{ balance }}€, careful with that budget!</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      name: 'Theo',
+      name: "Theo",
       id: 0,
-      location: 'Lille',
+      location: "Lille",
       weather_apikey: process.env.VUE_APP_API_KEY,
-      baseurl: 'https://api.openweathermap.org/data/2.5/weather?q=',
+      baseurl: "https://api.openweathermap.org/data/2.5/weather?q=",
       weather: {},
-      weather_image: 'lille.jpg',
-      balance: null
-    }
+      weather_image: "lille.jpg",
+      balance: null,
+    };
   },
   methods: {
     greeting() {
@@ -41,24 +52,22 @@ export default {
       let hour = date_object.getHours();
       let greeting;
 
-      if (hour >= 14 && hour < 18)
-        greeting = 'Good afternoon ';
-      else if (hour >= 18 || hour >= 0 && hour < 9)
-        greeting = 'Good night ';
-      else
-        greeting = 'Good morning ';
+      if (hour >= 14 && hour < 18) greeting = "Good afternoon ";
+      else if (hour >= 18 || (hour >= 0 && hour < 9)) greeting = "Good night ";
+      else greeting = "Good morning ";
       this.$refs.greeting.innerHTML = greeting + this.name;
     },
     get_weather() {
-      this.weather = fetch(`${this.baseurl}${this.location}&units=metric&appid=${this.weather_apikey}`)
-        .then(res => {
-          if (res.status != 200)
-            return 'error';
+      this.weather = fetch(
+        `${this.baseurl}${this.location}&units=metric&appid=${this.weather_apikey}`
+      )
+        .then((res) => {
+          if (res.status != 200) return "error";
           return res.json();
         })
         .then(this.setResults);
     },
-    setResults (results) {
+    setResults(results) {
       if (results == "error") {
         this.weather = {};
         return;
@@ -66,75 +75,85 @@ export default {
       this.weather = results;
     },
     get_balance() {
-      this.$http.
-      get('https://bankin-scraper.herokuapp.com/')
-      .then((response) => {
-        this.balance = response.data.balance;
-      }) 
-    }
+      this.$http
+        .get("https://bankin-scraper.herokuapp.com/")
+        .then((response) => {
+          this.balance = response.data.balance;
+        });
+    },
   },
   mounted() {
     this.greeting();
     this.get_weather();
     this.get_balance();
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  @import './assets/style/main.css';
-  @import './assets/style/mobile.css';
+@import "./assets/style/main.css";
+@import "./assets/style/mobile.css";
 
-  #greeting {
-    grid-area: a;
-    font-size: 1.7rem;
-    margin-top: 7rem;
-    font-weight: 600;
-  }
+#greeting {
+  grid-area: a;
+  font-size: 1.7rem;
+  margin-top: 8rem;
+  font-weight: 600;
+}
 
-  #weather {
-    grid-area: b;
-    color: black;
-    position: relative;
-  }
+#weather {
+  grid-area: b;
+  color: black;
+  position: relative;
+}
 
-  #weather_container {
-    position: relative;
-    margin-top: 5%;
-    background-color: rgba(0, 0, 0, 0.8);
-    width: 30%;
-    margin-left: 35%;
-    border-radius: 10%;
-  }
+#weather_container {
+  position: relative;
+  margin-top: 5%;
+  background-color: rgba(0, 0, 0, 0.8);
+  width: 30%;
+  margin-left: 35%;
+  border-radius: 10%;
+}
 
-  p {
-    color: white;
-    font-size: 1.3rem;
-    text-align: center;
-    font-weight: 400;
-    text-shadow: 1px 1.2px rgba(0, 0, 0, 0.25);
-  }
+p {
+  color: white;
+  font-size: 1.3rem;
+  text-align: center;
+  font-weight: 400;
+  text-shadow: 1px 1.2px rgba(0, 0, 0, 0.25);
+}
 
-  h2 {
-    text-align: center;
-    font-weight: 500;
-    color: white;
-    font-size: 1.9rem;
-  }
+h2 {
+  text-align: center;
+  font-weight: 500;
+  color: white;
+  font-size: 1.9rem;
+}
 
-  #weather_image {
-    opacity: 0.85;
-    pointer-events: none;
-    position: absolute;
-    width: 100%;
-    height: 25rem;
-    border-radius: 3%;
-    z-index: 0;
-    object-fit: cover;
-  }
+#weather_image {
+  opacity: 0.85;
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 25rem;
+  border-radius: 3%;
+  z-index: 0;
+  object-fit: cover;
+}
 
-  #bank {
-    grid-area: c;
-  }
+#bank {
+  grid-area: c;
+}
+
+#text_styling {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: -10vh;
+  margin-bottom: -12vh;
+  height: 35vh;
+  width: 35vh;
+}
 </style>
 
